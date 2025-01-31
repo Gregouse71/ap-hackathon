29e95ab24953cd2692b4a7bf354082b15dea463c
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2025-01-31 11:02:50 +0100

    creating window

--- main.jl
+++ main.jl
@@ -1,3 +1,9 @@
+using GLMakie, CairoMakie
+
+# activates a interactive window and creates a scene (= Figure())
+GLMakie.activate!()
+scene = Scene(camera = campixel!)
+
 function main()
     running = true
     while running
