f35184dbd376913938451806c55e46177e16ab77
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-01-31 10:01:25 +0100

    Correction structure GooTree

--- goo_tree.jl
+++ goo_tree.jl
@@ -2,11 +2,11 @@
     GooTree{N}
 Représente un arbre de goo:
 - *positions[2i:2i + 1]* est la position est la position du i ème goo
-- *aretes* est la matrice d'adjacence du graphe formé par les goos, avec pour coefficients les longueurs à vide des ressorts
+- *aretes* est la matrice d'adjacence du graphe formé par les goos: aretes[i] est la liste des voisins du i eme goo avec la longueur à vide associée
 
 """
 struct GooTree{N}
     positions::Vector{Float64}
-    aretes::Matrix{Float64}
+    edges::Vector{Vector{Tuple{Int, Float64}}}
 end
 
