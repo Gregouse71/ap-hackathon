902628cfb5b3d0e3650679b2a8f8cf81c3c29146
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2025-01-31 14:44:39 +0100

    screen to world conversion

--- display.jl
+++ display.jl
@@ -4,15 +4,11 @@ import GLMakie: text!, lift, scatter!, linesegments!
     createGameDisplay(scene::GLMakie.scene)
 """
 
-# constante de conversion : 1 m = 100 px
-m2px = 100
-
-
-function createGameDisplay(scene, pos_vel, attachs, edges)
-    current_positions = lift(extract_positions, pos_vel)
+function createGameDisplay(scene, pos_vel, attachs, edges, game_scene)
+    current_positions = lift(extract_positions, pos_vel, game_scene)
     
     # attach
-    attach_lines = lift(extract_attach, attachs, current_positions)
+    attach_lines = lift(extract_attach, attachs, current_positions, game_scene)
     linesegments!(scene, attach_lines)
 
     # edges
@@ -24,16 +20,16 @@ function createGameDisplay(scene, pos_vel, attachs, edges)
 
 end
 
-function extract_positions(pos_vel::Vector{Float64})
-    return [((pos_vel[i] + 2.) * m2px, (pos_vel[i+1] + 2.) * m2px) for i in 1:4:length(pos_vel)]
+function extract_positions(pos_vel::Vector{Float64}, game_scene)
+    return [world_to_screen((pos_vel[i], pos_vel[i+1]), game_scene.bounds) for i in 1:4:length(pos_vel)]
 end
 
-function extract_attach(attachs::Vector{Vector{Tuple{Tuple{Float64, Float64}, Float64}}}, positions::Vector{Tuple{Float64, Float64}})
+function extract_attach(attachs::Vector{Vector{Tuple{Tuple{Float64, Float64}, Float64}}}, positions::Vector{Tuple{Float64, Float64}}, game_scene)
     out = Tuple{Float64, Float64}[]
     for (i, la) in enumerate(attachs)
         for a in la
             push!(out, positions[i])
-            push!(out, (a[1] .+ (1., 2.)) .* m2px)
+            push!(out, world_to_screen(a[1], game_scene.bounds))
         end
     end
     out
@@ -50,7 +46,14 @@ function extract_edges(edges::Vector{Vector{Tuple{Int64, Float64}}}, positions::
     out
 end
 
-#=function to_screen_space(pos, bounds)
-    window_size = GLMakie.GLFW.GetWindowSize(glw)
+function screen_to_world(pos, bounds)
+    w, h = size(scene)
+    scale = min(w / (bounds[3] - bounds[1]), h / (bounds[4] - bounds[2]))
+    return (pos ./ scale) .+ (bounds[1], bounds[2])
+end
 
-end=#
+function world_to_screen(pos, bounds)
+    w, h = size(scene)
+    scale = min(w / (bounds[3] - bounds[1]), h / (bounds[4] - bounds[2]))
+    return (pos .- (bounds[1], bounds[2])) .* scale
+end
