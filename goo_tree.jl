8e01a18f8fea345a9bd22f15b852c67824bb5c60
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-01-31 12:54:52 +0100

    Geotree correct sim

--- goo_tree.jl
+++ goo_tree.jl
@@ -8,12 +8,12 @@ include("game.jl")
 Représente un arbre de goo:
 - *positions[4i:4i + 3]* est la position et la vitesse du i ème goo
 - *aretes* est la matrice d'adjacence du graphe formé par les goos: aretes[i] est la liste des voisins du i eme goo avec la longueur à vide associée
-- *attach* est la liste des points d'attache des goo: attach[i] est la liste des points auquel le i eme goo est attaché
+- *attach* est la liste des points d'attache des goo: attach[i] est la liste des points auquel le i eme goo est attaché associés au longueur à vide
 """
 struct GooTree
     positions::Vector{Float64}  # Position des goo dans l'espace des phases (q_ix, q_iy, p_ix, p_iy)
     edges::Vector{Vector{Tuple{Int64, Float64}}}
-    attach::Vector{Vector{Vector{Float64}}}
+    attach::Vector{Vector{Tuple{Vector{Float64}, Float64}}}
 end
 
 k = 100  # J/m²
@@ -23,7 +23,7 @@ k = 100  # J/m²
 
 Calcul, dans du, la dérivée de positions.
 """
-function step!(positions_derivee, positions::Vector{Float64}, params::Tuple{Vector{Vector{Tuple{Int64, Float64}}}, Vector{Vector{Vector{Float64}}}} , t)
+function step!(positions_derivee, positions::Vector{Float64}, params , t)
     adjacence, attach = params
     for i in 0:length(positions)÷4 - 1
         positions_derivee[4i + 1:4i + 2] = positions[4i + 3:4i + 4]  # la vitesse est la dérivée de la position
@@ -34,8 +34,9 @@ function step!(positions_derivee, positions::Vector{Float64}, params::Tuple{Vect
             δpos = pos .- positions[4i + 1:4i + 2]  # Difference de position des points
             ΣF .+= k * (1 - l0/norm(δpos)) .* δpos  # Force du ressort: loi de Hooke
         end
-        for att in attach
-            δpos = att .- positions[4i + 1:4i + 2]  # Difference de position des points
+        for attaché in attach[i + 1]
+            att, l0 = attaché
+            δpos = att[1] .- positions[4i + 1:4i + 2]  # Difference de position des points
             ΣF .+= k * (1 - l0/norm(δpos)) .* δpos  # Force du ressort: loi de Hooke
         end
         positions_derivee[4i + 3:4i + 4] = ΣF  # l'accélération est la dérivée de la vitesse
