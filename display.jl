083e0465f93064d5e03b13ca6952b2bb66467735
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2025-01-31 15:38:03 +0100

    colored lines

--- display.jl
+++ display.jl
@@ -4,25 +4,38 @@ import GLMakie: text!, lift, scatter!, linesegments!
     createGameDisplay(scene::GLMakie.scene)
 """
 
-function createGameDisplay(scene, pos_vel, attachs, edges, game_scene, screen_scene)
-    w, h = size(screen_scene)
+function createGameDisplay(scene, pos_vel, attachs, edges, platforms, game_scene, screen_scene)
     current_positions = lift(extract_positions, pos_vel, game_scene, screen_scene)
+    m2px = world_to_screen((1.0, 0.0), game_scene, screen_scene)[1]
     
+    # platforms
+    platfoms_lines = lift(extract_platforms, platforms, game_scene, screen_scene)
+    linesegments!(scene, platfoms_lines, linewidth = 0.05*m2px, color = :black, linecap=:round)
+
     # attach
     attach_lines = lift(extract_attach, attachs, current_positions, game_scene, screen_scene)
-    linesegments!(scene, attach_lines)
+    linesegments!(scene, attach_lines, linewidth = 0.03*m2px, color = :yellow, linecap=:round)
 
     # edges
     attach_edges = lift(extract_edges, edges, current_positions)
-    linesegments!(scene, attach_edges)
+    linesegments!(scene, attach_edges, linewidth = 0.03*m2px, color = :orange, linecap=:round)
 
     # Goos
-    scatter!(scene, current_positions, color = :black)
-
+    scatter!(scene, current_positions, markersize = 0.1*m2px, color = :black)
 end
 
 function extract_positions(pos_vel::Vector{Float64}, game_scene, screen_scene)
     return [world_to_screen((pos_vel[i], pos_vel[i+1]), game_scene, screen_scene) for i in 1:4:length(pos_vel)]
+end
+
+function extract_platforms(platforms, game_scene, screen_scene)
+    out = Tuple{Float64, Float64}[]
+    for plt in platforms
+        push!(out, world_to_screen(plt.ends[1], game_scene, screen_scene))
+        push!(out, world_to_screen(plt.ends[2], game_scene, screen_scene))
+    end
+    return out
+    out
 end
 
 function extract_attach(attachs::Vector{Vector{Tuple{Tuple{Float64, Float64}, Float64}}}, positions::Vector{Tuple{Float64, Float64}}, game_scene, screen_scene)
