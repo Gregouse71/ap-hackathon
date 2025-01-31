06964fdf99b2b91605e58de9e8775eab94b93240
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2025-01-31 14:26:41 +0100

    edges and attachs display

--- display.jl
+++ display.jl
@@ -1,4 +1,4 @@
-import GLMakie: text!, lift, scatter!
+import GLMakie: text!, lift, scatter!, linesegments!
 
 """
     createGameDisplay(scene::GLMakie.scene)
@@ -8,14 +8,46 @@ import GLMakie: text!, lift, scatter!
 m2px = 100
 
 
-function createGameDisplay(scene, pos_vel)
-    #text!(scene, 100, 100, text=lift(t -> string(count[]), count))
+function createGameDisplay(scene, pos_vel, attachs, edges)
     current_positions = lift(extract_positions, pos_vel)
+    
+    # attach
+    attach_lines = lift(extract_attach, attachs, current_positions)
+    linesegments!(scene, attach_lines)
+
+    # edges
+    attach_edges = lift(extract_edges, edges, current_positions)
+    linesegments!(scene, attach_edges)
+
+    # Goos
     scatter!(scene, current_positions, color = :black)
+
 end
 
 function extract_positions(pos_vel::Vector{Float64})
     return [((pos_vel[i] + 1.) * m2px, (pos_vel[i+1] + 2.) * m2px) for i in 1:4:length(pos_vel)]
+end
+
+function extract_attach(attachs::Vector{Vector{Tuple{Tuple{Float64, Float64}, Float64}}}, positions::Vector{Tuple{Float64, Float64}})
+    out = Tuple{Float64, Float64}[]
+    for (i, la) in enumerate(attachs)
+        for a in la
+            push!(out, positions[i])
+            push!(out, (a[1] .+ (1., 2.)) .* m2px)
+        end
+    end
+    out
+end
+
+function extract_edges(edges::Vector{Vector{Tuple{Int64, Float64}}}, positions::Vector{Tuple{Float64, Float64}})
+    out = Tuple{Float64, Float64}[]
+    for (i, links) in enumerate(edges)
+        for l in links
+            push!(out, positions[i])
+            push!(out, positions[l[1]])
+        end
+    end
+    out
 end
 
 #=function to_screen_space(pos, bounds)
--- main.jl
+++ main.jl
@@ -1,18 +1,23 @@
 import GLMakie: activate!, Scene, campixel!, events, Events, Observable, on
 include("display.jl")
+include("goo_tree.jl")
 
 function main()
     tree = GooTree([-1., 0., 0., 0., 1., 0., 0., 0.], [[(2, 1)], [(1, 1)]], [[((-1., -1.), 1.)], [((1., -1.), 1.)]])
     sol = simulate_tree(tree, (0., 10.))
     
-    positions = Observable(sol(0))
+    positions = Observable(tree.positions)
+    attachs = Observable(tree.attach)
+    edges = Observable(tree.edges)
+
+    println(tree.attach)
 
     # activates a interactive window and creates a scene (= Figure())
     activate!()
     scene = Scene(camera = campixel!)
 
     # renders interactively the whole game
-    createGameDisplay(scene, positions)
+    createGameDisplay(scene, positions, attachs, edges)
 
     on(events(scene).tick) do tick
         # update function
